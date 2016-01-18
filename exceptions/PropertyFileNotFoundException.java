586145a039cc3e11ddb7b06d1e475de28dbaed45
etharion <warlordcp@gmail.com>
2016-01-18 11:52:15 +0100

    exceptions added

---
+++ exceptions/PropertyFileNotFoundException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class PropertyFileNotFoundException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PropertyFileNotFoundException(String message) {
+		super(message);
+	}
+
+}
