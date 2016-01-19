b7ecf390e129dd2045d71ed6649fe4cca285a913
zeltr <zeltr@DESKTOP-J0IIDR8.ceuherning.int>
2016-01-19 11:48:16 +0100

    Opdatering (Slettet TODO)

--- exceptions/PersistenceConnectionFailureException.java
+++ exceptions/PersistenceConnectionFailureException.java
@@ -2,7 +2,6 @@ package exceptions;
 
 public class PersistenceConnectionFailureException extends Exception {
 	
-	//TODO
 	private static final long serialVersionUID = 1L;
 
 	public PersistenceConnectionFailureException(String message) {
