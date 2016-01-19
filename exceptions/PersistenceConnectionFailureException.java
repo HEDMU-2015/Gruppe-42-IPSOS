00f2450edd651f18d9f8604a491782c3d9065744
zeltr <zeltr@DESKTOP-J0IIDR8.ceuherning.int>
2016-01-19 11:05:32 +0100

    Connection Exception

--- exceptions/PersistenceConnectionFailureException.java
+++ exceptions/PersistenceConnectionFailureException.java
@@ -2,6 +2,7 @@ package exceptions;
 
 public class PersistenceConnectionFailureException extends Exception {
 	
+	//TODO
 	private static final long serialVersionUID = 1L;
 
 	public PersistenceConnectionFailureException(String message) {
