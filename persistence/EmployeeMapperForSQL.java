69d8d922389ac1cedaa539217d79769a63809c6a
etharion <warlordcp@gmail.com>
2016-01-19 09:16:39 +0100

    persistence added

---
+++ persistence/EmployeeMapperForSQL.java
@@ -0,0 +1,23 @@
+package persistence;
+
+import java.awt.List;
+import java.sql.PreparedStatement;
+
+import exceptions.PersistenceFailureException;
+
+public interface EmployeeMapperForSQL {
+
+	public List<Employee> findEmployee(List<Skill> skills, DataAccess da) throws PersistenceFailureException;
+	
+	public List<Employee> findEmployeeByName(String name, DataAccess da) throws PersistenceFailureException;
+	
+	public void addEmployeeSkill(Skill skill) throws PersistenceFailureException;
+	
+	public void setBindValues(Skills skills, PreparedStatement statement);
+	
+	public void createBindParams(Skills skills);
+	
+	public void removeEmployeeSkill(EmployeeProfile ep) throws PersistenceFailureException;
+	
+	public EmployeeProfile fetchEmployeeProfile(int id, DataAccess da) throws PersistenceFailureException;
+}
