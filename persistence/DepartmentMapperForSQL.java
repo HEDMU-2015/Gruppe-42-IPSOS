e6a194f6904414c6f854c66c2bffcfd364b9d02f
etharion <warlordcp@gmail.com>
2016-01-19 09:55:12 +0100

    Deparment/skill update

--- persistence/DepartmentMapperForSQL.java
+++ persistence/DepartmentMapperForSQL.java
@@ -5,13 +5,12 @@ import exceptions.PersistenceFailureException;
 
 public interface DepartmentMapperForSQL {
 	
-	public void createDepartment(department,DataAcces da) throws PersistenceFailureException;
+	public void createDepartment(Department department,DataAcces da) throws PersistenceFailureException;
 	
-	public void deleteDepartment(department, DataAcces da) throws PersistenceFailureException;
+	public void deleteDepartment(Department department, DataAcces da) throws PersistenceFailureException;
 	
-	public void updateDepartment(department, DataAcces da) throws PersistenceFailureException;
+	public void updateDepartment(Department department, DataAcces da) throws PersistenceFailureException;
 	
 	public List<Departments> fetchAllDepartments(DataAcces da)throws PersistenceFailureException;
-		
 	
 }
--- persistence/SkillMapperForSQL.java
+++ persistence/SkillMapperForSQL.java
@@ -4,10 +4,10 @@ import exceptions.PersistenceFailureException;
 
 public interface SkillMapperForSQL {
 
-	public void createSkill (skill,DataAcces da) throws PersistenceFailureException;
+	public void createSkill (Skill skill,DataAcces da) throws PersistenceFailureException;
 	
-	public void deleteSkill(skill, DataAcces da) throws PersistenceFailureException;
+	public void deleteSkill(Skill skill, DataAcces da) throws PersistenceFailureException;
 	
-	public void updateSkill(skill,DataAcces da) throws PersistenceFailureException;
+	public void updateSkill(Skill skill,DataAcces da) throws PersistenceFailureException;
 		
 }
