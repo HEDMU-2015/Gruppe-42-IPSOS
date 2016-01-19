8fb3d66b063d2dcb13cc99157b4e52b5ebe75462
Mcairborne <Mcairborne1000@gmail.com>
2016-01-19 12:37:32 +0100

    Department added (Not done)

--- domain/Department.java
+++ domain/Department.java
@@ -2,4 +2,30 @@ package domain;
 
 public class Department {
 
+	String name;
+	String parent_id;
+	int id;
+	
+	
+	public Department(int id, String name, String parent_id) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getParentId() {
+		return parent_id;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	
+	
+	
+	
+
 }
