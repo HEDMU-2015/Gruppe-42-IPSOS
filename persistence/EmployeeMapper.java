b972eb6e23c3afabe449ebe7616b157644694979
etharion <warlordcp@gmail.com>
2016-01-19 10:15:52 +0100

    controller, department, employee, skill update domain added

--- persistence/EmployeeMapper.java
+++ persistence/EmployeeMapper.java
@@ -1,21 +1,23 @@
 package persistence;
 
-import java.awt.List;
+import java.util.List;
 import java.sql.PreparedStatement;
 
+import domain.EmployeeProfile;
+import domain.Skill;
 import exceptions.PersistenceFailureException;
 
 public interface EmployeeMapper {
 
-	public List<Employee> findEmployee(List<Skill> skills, DataAccess da) throws PersistenceFailureException;
+	public List findEmployee(List skills, DataAccess da) throws PersistenceFailureException;
 	
-	public List<Employee> findEmployeeByName(String name, DataAccess da) throws PersistenceFailureException;
+	public List findEmployeeByName(String name, DataAccess da) throws PersistenceFailureException;
 	
 	public void addEmployeeSkill(Skill skill) throws PersistenceFailureException;
 	
-	public void setBindValues(Skills skills, PreparedStatement statement);
+	public void setBindValues(List skills, PreparedStatement statement);
 	
-	public void createBindParams(Skills skills);
+	public void createBindParams(List skills);
 	
 	public void removeEmployeeSkill(EmployeeProfile ep) throws PersistenceFailureException;
 	
--- persistence/SkillMapper.java
+++ persistence/SkillMapper.java
@@ -1,10 +1,11 @@
 package persistence;
 
+import domain.Skill;
 import exceptions.PersistenceFailureException;
 
 public interface SkillMapper {
 
-	public void createSkill (Skill skill,DataAcces da) throws PersistenceFailureException;
+	public void createSkill(Skill skill,DataAcces da) throws PersistenceFailureException;
 	
 	public void deleteSkill(Skill skill, DataAcces da) throws PersistenceFailureException;
 	
